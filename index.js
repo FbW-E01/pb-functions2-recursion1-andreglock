cd089ce73945b2ca7f5dd2586b5ae53286f9e8b6
André <andreglock@gmail.com>
2021-05-10 14:11:55 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function sum(number) {
+/* function sum(number) { // Non recursive solution
     return number.reduce(function(accumulator, current) {
         return accumulator + current;
     });
+}; */
+
+function sum(numbers) {
+    const lastNumber = numbers.pop();
+    if (lastNumber === undefined) { return 0 }
+    return lastNumber + sum(numbers);
 };
 
 const array = [1, 2, 3, 4, 5, 6];
